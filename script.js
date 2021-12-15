836338649ab3912b90b7b1b807db56a5c49f5c34
James Ram <93205091+JamesRamEchano@users.noreply.github.com>
2021-12-15 12:50:23 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.typing',{
+    string:["Adamsonian Student","Gamer", "Basketball Player"],
+    backspeed: 60,
+    typespeed: 100,
+    loop: true,
+});
+
+var typed = new Typed('.typing2',{
+    tring:["Adamsonian Student","Gamer", "Basketball Player"],
+    backspeed: 60,
+    typespeed: 100,
+    loop: true,
+});
